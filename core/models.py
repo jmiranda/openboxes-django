fd8fc2afa520f7c7882b49f9e8920d41693d0d03
Justin Miranda <jcm62@columbia.edu>
2017-01-17 16:31:35 -0600

    added unique constraint to code property

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class Category(models.Model):
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -15,17 +15,13 @@ class Category(models.Model):
 
 class Product(models.Model):
 
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=255)
     description = models.TextField()
     published_date = models.DateTimeField(default=timezone.now)
     created_date = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey('auth.User')
-    category = models.ForeignKey(Category, models.SET_NULL, null = True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
