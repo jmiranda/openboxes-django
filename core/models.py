4b0e62f7556aaac70cd90425019fedd9ba1d1bac
Justin Miranda <jcm62@columbia.edu>
2017-03-09 09:33:43 -0600

    added unit of measure to product model

--- core/models.py
+++ core/models.py
@@ -37,18 +37,17 @@ class InventoryItem(models.Model):
     expiration_date = models.DateField(null=True)
 
 
-
 class AbstractProduct(models.Model):
     class Meta:
         abstract = True
 
     code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=255)
+    unit_of_measure = models.CharField(max_length=255, default=None, null=True)
     description = models.TextField()
     published_date = models.DateTimeField(default=timezone.now)
     created_date = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey('auth.User')
-
     thumbnail_url = models.TextField(default='https://chipinworld.com/images/product/default_product.png')
     photo_url = models.TextField(default='https://chipinworld.com/images/product/default_product.png')
 
